e1a0d3a4973ca93082c5ceae440c0d9e351eda42
JUUN01\Martin Ouwehand <martin.ouwehand@juunsoftware.nl>
2023-09-24 00:54:30 +0200

    test

--- Msoft.Snake/Msoft.Snake/Program.cs
+++ Msoft.Snake/Msoft.Snake/Program.cs
@@ -1,11 +1,10 @@
-﻿using Msoft.Snake.Levels;
-
-namespace Msoft.Snake
+﻿namespace Msoft.Snake
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+            //Start from here
             Console.CursorVisible = false;
             var game = new Game(100, new Level1());
             game.Start();
